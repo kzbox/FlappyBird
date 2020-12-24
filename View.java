e586b77d872882222a1bf1abfcb008a44a7bb5ff
temimi-bit <goldsoul.keito@gmail.com>
2020-12-25 00:53:28 +0900

    add button nado

--- View.java
+++ View.java
@@ -3,8 +3,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-aaa
-
 // View
 class ViewPanel extends JPanel implements Observer {
     private ModelObservable model;
@@ -19,15 +17,24 @@ class ViewPanel extends JPanel implements Observer {
         this.setFocusable(true);
     }
     public void paintComponent(Graphics g){
+    JButton b = new JButton("YES");
+    JLabel l = new JLabel("CONTINUE?");
         super.paintComponent(g);
-        if(model.getGameOverFlag()){
-            g.fillRect(100, 100, 200, 200);
-        }
         model.bird.draw(g);
         for(int i = 0; i < model.DOKAN_BUF; i++){
             model.upperDokan.get(i).draw(g);
             model.lowerDokan.get(i).draw(g);
         }
+        g.setColor(Color.black);
+        if(model.getGameOverFlag()){
+            g.fillRect(100, 100, 200, 200);
+            b.setBounds(130,240,60,30);
+            l.setBounds(165,130,180,80);
+            l.setForeground(Color.white);
+            this.add(b);
+            this.add(l);
+        }
+        
     }
     public void update(Observable o, Object obj){
         repaint();

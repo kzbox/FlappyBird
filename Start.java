ed649402ab3a83c9235033b09abdbc352a066bd4
yokochannn <kosuke235711@gmail.com>
2021-01-08 22:09:34 +0900

    add score

--- Start.java
+++ Start.java
@@ -2,13 +2,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.io.*;
 // view
 class StartPanel extends JPanel {
     ModelObservable model;
     StartController cont;
     JButton b;
     JLabel c;
+    JLabel score;
     Image img;
+    String high_score;
+
+    public void Score(){
+        try{
+            File file = new File("../log/high_score.txt");
+            if(file.exists()){
+                FileReader filereader = new FileReader(file);
+                BufferedReader br = new BufferedReader(filereader);
+        
+                high_score = br.readLine();
+        
+                filereader.close();
+            }
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
     public StartPanel(ModelObservable mo, StartController co){
         model = mo;
         cont = co;
@@ -16,7 +36,8 @@ class StartPanel extends JPanel {
         b = new JButton("Start!!!!");
         //b.setPreferredSize(new Dimension(200, 100));
         b.setFont(new Font("MS ゴシック", Font.BOLD, 24));
-        b.setBounds(ModelObservable.SCREEN_WIDTH/4, ModelObservable.SCREEN_HEIGHT/2, ModelObservable.SCREEN_WIDTH/2, ModelObservable.SCREEN_HEIGHT/7);
+        b.setBounds(ModelObservable.SCREEN_WIDTH/4, ModelObservable.SCREEN_HEIGHT/2, 
+                    ModelObservable.SCREEN_WIDTH/2, ModelObservable.SCREEN_HEIGHT/7);
         b.setForeground(Color.YELLOW);
         b.setBackground(Color.BLUE);
         b.addActionListener(cont);
@@ -26,16 +47,20 @@ class StartPanel extends JPanel {
         c.setForeground(Color.RED);
         c.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 24));
 
-        img = Toolkit.getDefaultToolkit().getImage("startview.jpg");
-        
+        img = Toolkit.getDefaultToolkit().getImage("../images/startview.jpg");
+
+        score = new JLabel("HIGH SCORE:" + high_score);
+        score.setBounds(130, 600, 200, 50);
+        score.setForeground(Color.RED);
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawImage(img, 0, 0, ModelObservable.SCREEN_WIDTH, ModelObservable.SCREEN_WIDTH, 
-        500, 300, ModelObservable.SCREEN_WIDTH+500, ModelObservable.SCREEN_HEIGHT+300, this);
+        g.drawImage(img, 0, 0, this);
         this.add(c);
         this.add(b);
+        this.add(score);
     }
+
 }
 
 // controller
